ed11f0e18af7013ef7eaf0f76068c9c458dd40f7
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-11-08 16:11:00 -0600

    mousedown on About Us red, mouseup on About Us black

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+//1st event - mousedown on "About Us" red, mouseup on "About Us" black
+const aboutUsNav = document.querySelector('a:nth-of-type(2)')
+aboutUsNav.addEventListener('mousedown', event => event.target.style.color = 'red')
+aboutUsNav.addEventListener('mouseup', event => event.target.style.color = 'black')
