1b10e27a4267c0aab51934b61e69ae58e6d3bdf9
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-11-08 16:46:22 -0600

    mouseover on Fun Bus font size changed to 6rem, mouseout changed back to 4rem

--- js/index.js
+++ js/index.js
@@ -4,3 +4,8 @@
 const aboutUsNav = document.querySelector('a:nth-of-type(2)')
 aboutUsNav.addEventListener('mousedown', event => event.target.style.color = 'red')
 aboutUsNav.addEventListener('mouseup', event => event.target.style.color = 'black')
+
+//2nd event - mouseover on "Fun Bus" font size changed to 6rem, mouseout changed back to 4rem
+const funBusH1 = document.querySelector('h1')
+funBusH1.addEventListener('mouseover', event => event.target.style.fontSize = '6rem')
+funBusH1.addEventListener('mouseout', event => event.target.style.fontSize = '4rem')
